6716ab7ffe8d8e024496e20c4ae5deff4110112f
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-04-08 20:13:33 -0700

    Bump the version of bpf-tools

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ fn llvm_home() -> Result<PathBuf, anyhow::Error> {
     Ok(home_dir
         .join(".cache")
         .join("solana")
-        .join("v1.22")
+        .join("v1.24")
         .join("bpf-tools")
         .join("llvm"))
 }
