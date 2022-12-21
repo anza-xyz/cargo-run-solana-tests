eb8f22df8954ce4278a12c30bd1ba396a7aad7a3
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-12-21 11:19:10 -0500

    Bump sbf-tools version

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ fn llvm_home() -> Result<PathBuf, anyhow::Error> {
     Ok(home_dir
         .join(".cache")
         .join("solana")
-        .join("v1.31")
+        .join("v1.32")
         .join("sbf-tools")
         .join("llvm"))
 }
